58e1ef0fd1627db57d19785586e422f9533e9d5f
Aaron Sikes <aaron@sikes.io>
2016-10-04 20:48:07 -0400

    move frogs when you click them

--- main.js
+++ main.js
@@ -11,3 +11,41 @@ function render(frogs) {
 }
 
 render(frogs);
+
+$(document).delegate('.left-frog', 'click',
+    function(event){
+      var index = $(this).index();
+
+      if (frogs[index + 1] === 'no') {
+        swap(frogs, index, index + 1)
+      }
+      else if (frogs[index + 2] === 'no') {
+        swap(frogs, index, index + 2)
+      }
+
+      render(frogs)
+    }
+)
+
+$(document).delegate('.right-frog', 'click',
+    function(event){
+      var index = $(this).index();
+
+      if (frogs[index - 1] === 'no') {
+        swap(frogs, index, index - 1)
+      }
+      else if (frogs[index - 2] === 'no') {
+        swap(frogs, index, index - 2)
+      }
+
+      render(frogs)
+    }
+)
+
+function swap(array, index_a, index_b) {
+  var a = array[index_a];
+  var b = array[index_b];
+
+  array[index_a] = b;
+  array[index_b] = a;
+}
